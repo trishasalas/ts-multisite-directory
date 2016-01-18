c7601772969acb8d3f8032d92234f03f221cafa4
trishasalas <trisha@trishasalas.com>
2016-01-18 12:07:01 -0600

    changes to make testing easier and to adhere to wp coding standards

--- ts-ms-dir.php
+++ ts-ms-dir.php
@@ -27,28 +27,30 @@ class TS_Multisite_Directory {
 		$site_id,
 		$sites,
 		$site,
-		$url,
-		$output;
+		$url;
 
 
 	public function __construct() {
-		add_action( 'init', array( $this, 'ts_multisite_info' ) );
-		add_action( 'init', array( $this, 'ts_multisite_directory' ) );
+		$this->init();
+	}
+
+	public function init() {
+		$this->ts_multisite_info();
 		add_action( 'init', array( $this, 'load_styles' ) );
 		add_action( 'init', array( $this, 'ts_multisite_directory_i18n' ) );
 		add_action( 'insert_blog', 'delete_sites_info_transient' );
 		add_shortcode('multisite_directory', array( $this, 'ts_multisite_directory' ) );
-
 	}
 
-	function ts_multisite_directory_i18n() {
+	public function ts_multisite_directory_i18n() {
 		load_plugin_textdomain( 'ts-multisite-directory', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/'  );
 	}
-	function load_styles() {
+
+	public function load_styles() {
 		wp_enqueue_style('ts-multisite-directory-styles', plugins_url( 'assets/style.css', __FILE__ ), null, null, 'screen');
 	}
 
-	function ts_multisite_info() {
+	public  function ts_multisite_info() {
 		global $wpdb;
 		$this->sites_info = get_transient( 'ts_multisite_info' );
 
@@ -78,18 +80,18 @@ class TS_Multisite_Directory {
 	}
 
 	public function ts_multisite_directory() {
-		$this->output = '';
-		$this->output .= '<div id="multisite-directory">';
+		$output = '';
+		$output .= '<div id="multisite-directory">';
 		foreach ( $this->sites_info as $this->site_info ) {
-			$this->output .= '<h3><a href="' . esc_url( $this->site_info['url'] ) . '">' . esc_html( $this->site_info['name'] ) . '</a></h3>';
-			$this->output .= '<div class="posts-rss"><a href="' . esc_url( $this->site_info['rss'] ) . '">' . __( 'RSS for Posts', 'multisite-directory' ) . '</a></div>';
-			$this->output .= '<div class="comments-rss"><a href="' . esc_url( $this->site_info['comments_rss'] ) . '">' . __( 'RSS for Comments', 'multisite-directory' ) . '</a></div></ul>';
+			$output .= '<h3><a href="' . esc_url( $this->site_info['url'] ) . '">' . esc_html( $this->site_info['name'] ) . '</a></h3>';
+			$output .= '<div class="posts-rss"><a href="' . esc_url( $this->site_info['rss'] ) . '">' . __( 'RSS for Posts', 'multisite-directory' ) . '</a></div>';
+			$output .= '<div class="comments-rss"><a href="' . esc_url( $this->site_info['comments_rss'] ) . '">' . __( 'RSS for Comments', 'multisite-directory' ) . '</a></div></ul>';
 		}
-		$this->output .= '</div>';
-		return $this->output;
+		$output .= '</div>';
+		return $output;
 	}
 
-	function delete_sites_info_transient() {
+	public function delete_sites_info_transient() {
 		delete_transient( 'ts_multisite_info' );
 	}
 
